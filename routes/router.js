76666e6e4d928fea7259f569132827e587fa7d39
Francisco <fco.ochoas@gmail.com>
2016-08-18 13:53:25 -0700

    changes

--- routes/router.js
+++ routes/router.js
@@ -21,7 +21,7 @@ module.exports = function(express){
 
             //     res.render('index', { images : files });
             // });
-            res.end('done');
+            res.render('index');
         });
     
 
